677fcd5a09bfda19f6fff3cd8bdee6d07fb7aa8a
Kate Anderson <kate-anderson@uiowa.edu>
2018-04-19 21:21:42 -0500

    added proof of work and validation

--- src/main/kotlin/Block.kt
+++ src/main/kotlin/Block.kt
@@ -2,7 +2,7 @@ package hawkoin
 
 import java.util.*
 
-class Block (val index:Int, val previousHash:String, val data:Any){
+class Block (val index:Int, val previousHash:String, val data:Any, val proofOfWork:Int){
     val timestamp:Long = Date().time
     val hash = hashing((index.toString() + previousHash + timestamp + data))
 }
--- src/main/kotlin/Blockchain.kt
+++ src/main/kotlin/Blockchain.kt
@@ -6,7 +6,7 @@ object Blockchain {
         get() = chain.last()
 
     init {
-        chain.add(Block(0,"0","Genesis Block"))
+        chain.add(Block(0,"0","Genesis Block",0))
     }
 
     fun addNewBlock(block:Block){
@@ -14,4 +14,14 @@ object Blockchain {
     }
 
     private fun isNewBlockValid(newBlock:Block):Boolean = ((newBlock.index == latestBlock.index+1)||(newBlock.previousHash== latestBlock.hash))
+
+    private fun generatePOW(previousPOW:Int, difficulty:Int=1):Int{
+        var proof = previousPOW +1
+        val nonce = 18*difficulty
+
+        while ((proof+previousPOW)%nonce != 0){
+            proof +=1
+        }
+        return proof
+    }
 }
